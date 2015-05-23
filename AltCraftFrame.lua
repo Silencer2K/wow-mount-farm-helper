f5e0d6f5ccf0cac5c15d1fcae218938baf7ba789
Silencer <silencer2k@gmail.com>
2015-05-23 17:09:41 +0000

    init scroll

--- AltCraftFrame.lua
+++ AltCraftFrame.lua
@@ -2,10 +2,14 @@ local addonName, addon = ...
 
 local L = LibStub('AceLocale-3.0'):GetLocale(addonName)
 
+local LIST_SCROLL_ITEM_HEIGHT = 60
+
 local frame = AltCraftMFHTabFrame
 
 function frame:OnInitialize()
     self.Title:SetText("Mount Farm Helper")
+
+    self.ListScroll:OnInitialize()
 end
 
 function frame:OnShow()
@@ -25,5 +29,30 @@ end
 function frame:Update(what)
     if what then
         return
+    end
+
+    self.ListScroll:OnInitialize()
+end
+
+function frame.ListScroll:OnInitialize()
+    self.scrollBar.doNotHide = 1
+
+    HybridScrollFrame_OnLoad(self)
+    self.update = function() self:Update() end
+
+    HybridScrollFrame_CreateButtons(self, 'AltCraftMFHButtonTemplate', 0, 0)
+    self:Update()
+end
+
+function frame.ListScroll:Update()
+    local numRows = 20
+
+    HybridScrollFrame_Update(self, numRows * LIST_SCROLL_ITEM_HEIGHT, self:GetHeight())
+
+    local scrollOffset = HybridScrollFrame_GetOffset(self)
+
+    local i
+    for i = 1, #self.buttons do
+        local button = self.buttons[i]
     end
 end
